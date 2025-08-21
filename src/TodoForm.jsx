d673faa0b1f82fbec2bd1744b8666ef2107d5144
Jennifer McDougall <jenniferannvancura@gmail.com>
2025-08-21 12:58:39 -0500

    week 5

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,21 +1,24 @@
 import { useRef } from "react"
+import { useState } from 'react'
 
 function TodoForm({onAddTodo}){
+    const [workingTodoTitle, setWorkingTodo] = useState("");
     const todoTitleInput = useRef("")
 
     function handleAddTodo(event){
         event.preventDefault()
-        const title = event.target.title.value
-        onAddTodo(title)
-        event.target.title.value = ""
+        onAddTodo(workingTodoTitle)
+        setWorkingTodo("")
         todoTitleInput.current.focus();
     }
 
     return(     
     <form onSubmit={handleAddTodo}>
     <label htmlFor="todo-input">Todo:</label>
-    <input type="text" id="todoTitle" name="title" ref={todoTitleInput}/>
-    <button> Add Todo </button>
+    <input type="text" id="todoTitle" name="title" ref={todoTitleInput} value= {workingTodoTitle}            
+    onChange={(event) => setWorkingTodo(event.target.value)}
+    />
+    <button disabled= {!workingTodoTitle}> Add Todo </button>
     </form>
     );
 }
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,5 +1,16 @@
-function ToDoListItem({todo}){
-    return <li>{todo.title}</li>
+function TodoListItem({ todo, onCompleteTodo }) {
+  return (
+    <li>
+      <form>
+        <input
+           type= "checkbox"
+           checked= {todo.isCompleted}
+           onChange={() => onCompleteTodo(todo.id)}
+        />
+        {todo.title}
+      </form>
+    </li>
+  );
 }
 
-export default ToDoListItem
+export default TodoListItem;
